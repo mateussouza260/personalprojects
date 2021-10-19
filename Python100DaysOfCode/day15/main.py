6f6165caf41e9008f6333fc6f51b08bd265d520c
mateussouza260 <70783690+mateussouza260@users.noreply.github.com>
2021-10-18 21:57:18 -0300

    creating functions
    Creating functions for a clearer vision of the code

--- Python100DaysOfCode/day15/main.py
+++ Python100DaysOfCode/day15/main.py
@@ -3,15 +3,30 @@ from drinksMenu import MENU, resources
 
 def check_resources(drink):
     if MENU[drink]["ingredients"]["water"] > resources["water"]:
-        return 1
+        return "NoWater"
     elif MENU[drink]["ingredients"]["coffee"] > resources["coffee"]:
-        return 3
+        return "NoCoffee"
     elif userChoice != "espresso":
         if MENU[drink]["ingredients"]["milk"] > resources["milk"]:
-            return 2
+            return "NoMilk"
         else:
-            return 0
+            return "AllGood"
     else:
+        return "AllGood"
+
+
+def resolve_resources(drink):
+    if resources_es == "AllGood":
+        calc_money(drink)
+        return MENU[drink]["cost"]
+    elif resources_es == "NoWater":
+        print("Sorry, water")
+        return 0
+    elif resources_es == "NoMilk":
+        print("Sorry, milk")
+        return 0
+    elif resources_es == "NoCoffee":
+        print("Sorry, coffee")
         return 0
 
 
@@ -22,16 +37,21 @@ def subtract_resources(drink):
         resources["milk"] -= MENU[drink]["ingredients"]["milk"]
 
 
-def check_money():
+def calc_money(drink):
     quarters = int(input("How many quarters: ")) * 0.25
     dimes = int(input("How many dimes: ")) * 0.10
     nickles = int(input("How many nickles: ")) * 0.05
     pennies = int(input("How many pennies: ")) * 0.01
     money = (quarters + dimes + nickles + pennies)
-    return money
+    if money < MENU[drink]["cost"]:
+        print(f"Sorry that's not enough money. Money refunded:{money}.")
+    else:
+        change = money - MENU[drink]["cost"]
+        print(f"Here is ${round(change, 2)} in change\nHere is you latte, enjoy!")
+        subtract_resources(drink)
 
 
-totalMoney = 0
+totalMoney = 0.0
 is_on = True
 while is_on:
     money = 0.00
@@ -44,56 +64,10 @@ while is_on:
               f"Money: ${round(totalMoney, 2)}")
     elif userChoice == "espresso":
         resources_es = check_resources("espresso")
-        if resources_es == 0:
-            money = check_money()
-            if money < MENU["espresso"]["cost"]:
-                print(f"Sorry that's not enough money. Money refunded:{money}.")
-            else:
-                change = money - MENU["espresso"]["cost"]
-                print(f"Here is ${round(change, 2)} in change\nHere is you espresso, enjoy!")
-                subtract_resources(userChoice)
-
-                totalMoney += MENU["espresso"]["cost"]
-        elif resources_es == 1:
-            print("Sorry, water")
-        elif resources_es == 2:
-            print("Sorry, milk")
-        elif resources_es == 3:
-            print("Sorry, coffee")
-
+        totalMoney += resolve_resources(userChoice)
     elif userChoice == "latte":
         resources_es = check_resources("latte")
-        if resources_es == 0:
-            money = check_money()
-            if money < MENU["latte"]["cost"]:
-                print(f"Sorry that's not enough money. Money refunded:{money}.")
-            else:
-                change = money - MENU["latte"]["cost"]
-                print(f"Here is ${round(change, 2)} in change\nHere is you latte, enjoy!")
-                subtract_resources(userChoice)
-
-                totalMoney += MENU["latte"]["cost"]
-        elif resources_es == 1:
-            print("Sorry, water")
-        elif resources_es == 2:
-            print("Sorry, milk")
-        elif resources_es == 3:
-            print("Sorry, coffee")
+        totalMoney += resolve_resources(userChoice)
     elif userChoice == "cappuccino":
         resources_es = check_resources("cappuccino")
-        if resources_es == 0:
-            money = check_money()
-            if money < MENU["cappuccino"]["cost"]:
-                print(f"Sorry that's not enough money. Money refunded:{money}.")
-            else:
-                change = money - MENU["cappuccino"]["cost"]
-                print(f"Here is ${round(change, 2)} in change\nHere is you cappuccino, enjoy!")
-                subtract_resources(userChoice)
-
-                totalMoney += MENU["cappuccino"]["cost"]
-        elif resources_es == 1:
-            print("Sorry, water")
-        elif resources_es == 2:
-            print("Sorry, milk")
-        else:
-            print("Sorry, coffee")
+        totalMoney += resolve_resources(userChoice)
