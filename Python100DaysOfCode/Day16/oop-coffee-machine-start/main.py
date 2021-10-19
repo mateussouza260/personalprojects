353445f1edc444391846bac49e1ac7a2aa1ca2c0
mateussouza260 <70783690+mateussouza260@users.noreply.github.com>
2021-10-19 20:42:04 -0300

    Loop working

--- Python100DaysOfCode/Day16/oop-coffee-machine-start/main.py
+++ Python100DaysOfCode/Day16/oop-coffee-machine-start/main.py
@@ -1,3 +1,20 @@
 from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
+
+is_on = True
+while is_on:
+    available_drinks = Menu().get_items()
+    user_choice = input(f"What drink are you having({available_drinks}): ").lower()
+    if user_choice == "report":
+        CoffeeMaker().report()
+        MoneyMachine().report()
+    elif user_choice == "off":
+        is_on = False
+    else:
+        drink = Menu().find_drink(user_choice)
+        print(drink.ingredients)
+        if CoffeeMaker().is_resource_sufficient(drink):
+            if MoneyMachine().make_payment(drink.cost):
+                CoffeeMaker().make_coffee(drink)
+
