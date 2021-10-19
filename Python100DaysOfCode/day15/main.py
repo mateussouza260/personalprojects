4fdba60ce1718d152cffccedb146f33652ddc9c5
mateussouza260 <70783690+mateussouza260@users.noreply.github.com>
2021-10-18 22:51:14 -0300

    rearranging functions

--- Python100DaysOfCode/day15/main.py
+++ Python100DaysOfCode/day15/main.py
@@ -2,39 +2,21 @@ from drinksMenu import MENU, resources
 
 
 def check_resources(drink):
-    if MENU[drink]["ingredients"]["water"] > resources["water"]:
-        return "NoWater"
-    elif MENU[drink]["ingredients"]["coffee"] > resources["coffee"]:
-        return "NoCoffee"
-    elif userChoice != "espresso":
-        if MENU[drink]["ingredients"]["milk"] > resources["milk"]:
-            return "NoMilk"
+    needed_supply = MENU[drink]["ingredients"]
+    for item in needed_supply:
+        if needed_supply[item] > resources[item]:
+            print(f"Sorry, there is no {item}")
+            return False
         else:
-            return "AllGood"
-    else:
-        return "AllGood"
+            return True
 
 
-def resolve_resources(drink):
-    if resources_es == "AllGood":
-        calc_money(drink)
-        return MENU[drink]["cost"]
-    elif resources_es == "NoWater":
-        print("Sorry, water")
-        return 0
-    elif resources_es == "NoMilk":
-        print("Sorry, milk")
-        return 0
-    elif resources_es == "NoCoffee":
-        print("Sorry, coffee")
-        return 0
-
-
-def subtract_resources(drink):
+def make_coffee(drink):
     resources["water"] -= MENU[drink]["ingredients"]["water"]
     resources["coffee"] -= MENU[drink]["ingredients"]["coffee"]
     if drink == "latte" or drink == "cappuccino":
         resources["milk"] -= MENU[drink]["ingredients"]["milk"]
+    print(f"Here is you {drink}, enjoy!")
 
 
 def calc_money(drink):
@@ -43,12 +25,17 @@ def calc_money(drink):
     nickles = int(input("How many nickles: ")) * 0.05
     pennies = int(input("How many pennies: ")) * 0.01
     money = (quarters + dimes + nickles + pennies)
+    return money
+
+
+def check_transaction(drink):
     if money < MENU[drink]["cost"]:
         print(f"Sorry that's not enough money. Money refunded:{money}.")
+        return False
     else:
         change = money - MENU[drink]["cost"]
-        print(f"Here is ${round(change, 2)} in change\nHere is you latte, enjoy!")
-        subtract_resources(drink)
+        print(f"Here is ${round(change, 2)} in change")
+        return True
 
 
 totalMoney = 0.0
@@ -58,16 +45,26 @@ while is_on:
     userChoice = input("What would you like> (espresso/latte/cappuccino): ").lower()
     if userChoice == "off":
         is_on = False
-        break
     elif userChoice == "report":
-        print(f"Water: {resources['water']}\nMilk: {resources['milk']}\nCoffee: {resources['coffee']}\n"
-              f"Money: ${round(totalMoney, 2)}")
+        print(f"Water: {resources['water']}ml")
+        print(f"Milk: {resources['milk']}ml")
+        print(f"Coffee: {resources['coffee']}g")
+        print(f"Money: ${round(totalMoney, 2)}")
     elif userChoice == "espresso":
-        resources_es = check_resources("espresso")
-        totalMoney += resolve_resources(userChoice)
+        if check_resources("espresso"):
+            money = calc_money("espresso")
+            if check_transaction("espresso"):
+                totalMoney += MENU["espresso"]["cost"]
+                make_coffee("espresso")
     elif userChoice == "latte":
-        resources_es = check_resources("latte")
-        totalMoney += resolve_resources(userChoice)
+        if check_resources("latte"):
+            money = calc_money("latte")
+            if check_transaction("latte"):
+                totalMoney += MENU["latte"]["cost"]
+                make_coffee("latte")
     elif userChoice == "cappuccino":
-        resources_es = check_resources("cappuccino")
-        totalMoney += resolve_resources(userChoice)
+        if check_resources("cappuccino"):
+            money = calc_money("cappuccino")
+            if check_transaction("cappuccino"):
+                totalMoney += MENU["cappuccino"]["cost"]
+                make_coffee("cappuccino")
